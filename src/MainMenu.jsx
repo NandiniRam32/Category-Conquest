56fe062ced05e47ae07f2ec9b69823aa03be6118
Nandini Ramakrishnan <126700503+NandiniRam32@users.noreply.github.com>
2024-01-17 17:38:23 -0500

    Add files via upload

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -47,8 +47,8 @@ export const MainMenu = ({setCurrentPage, setPoints, setCurrentTeam, setClickedC
                 <div style={{background: "#c04dfa", height: "14vh", width: "14vw", marginLeft: "42vw", border: "0.5vw solid white",
                             fontFamily: "monospace", fontSize: "4vw", alignItems: "center", paddingTop: "4vh", 
                             marginTop: "2vh"}} onClick={() => handleSetPage("levels")} className="changeBgClrBlack"><b>Play</b></div>
-                <div className="changeBgClr" style={{height: "6vh", width: "32vw", marginLeft: "68vw", 
-                            fontFamily: "monospace", fontSize: "2vw", alignItems: "center", marginTop: "3.4vw"}} onClick={() => 
+                <div className="changeBgClr" style={{height: "4vh", width: "32vw", marginLeft: "68vw", 
+                            fontFamily: "monospace", fontSize: "2vw", alignItems: "center", marginTop: "2.8vw"}} onClick={() => 
                             handleBackgroundColorChange(backgroundColor)}><b>Change Background Color</b></div>
             </div>
         </>
